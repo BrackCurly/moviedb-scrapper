25f10f6db60956d719a322b263606121a0d0a835
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2015-01-14 17:12:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# moviedb-scraper
+
+Tool to create a neo4j dump from the [moviedb](http://www.themoviedb.org) API
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2015 Paul Sonnentag
+
+Distributed under the MIT License
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject moviedb-scraper "0.1.0-SNAPSHOT"
+  :description "Tool to create a neo4j dump from the moviedb API"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT License"
+            :url "https://github.com/BrackCurly/moviedb-scrapper"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clojurewerkz/neocons "3.1.0-beta2"]])
