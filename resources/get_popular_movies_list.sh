9b3fdb5353fc9c31883a6b48145b0f3cf021a0c7
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2015-01-17 09:53:45 +0100

    Fix get_popular_movies_list script

--- resources/get_popular_movies_list.sh
+++ resources/get_popular_movies_list.sh
@@ -1,10 +1,7 @@
 api_key=$(cat "api_key")
-
 cd movies
-
-for i in {1..1000}; do
-curl -o $i.json http://api.themoviedb.org/3/movie/popular?api_key=$api_key&page=$i;
+for i in {0..0}; do
+curl -o $i.json  "http://api.themoviedb.org/3/movie/popular?api_key=${api_key}&page=${i}";
 sleep 0.5s
 done
-
 cd ..
